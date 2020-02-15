ba1f08d9b6b4fd802570814ca4b120fc7c61cb7d
Mehdi Hamzehlou <hamzehlou.mehdi@gmail.com>
2020-02-15 17:31:41 +0330

    refact get Files Occurence Percentage

--- src/main/scala/bazoka/service/searchTextService.scala
+++ src/main/scala/bazoka/service/searchTextService.scala
@@ -10,19 +10,18 @@ object searchTextService {
   def getFilesOccurencePercentage(inMemoryFiles: Map[String, Map[String, Int]], wordsToSearch: List[String]): ListMap[String, Double] = {
 
     val FilesOccurence: Map[String, Double] = inMemoryFiles.map { case (key: String, value: Map[String, Int]) =>
-      val fileName: String = key
-      val fileOccurencePercentage = getFileOccurencePercentage(wordsToSearch.foldLeft(0) { case (acc, w) =>
-        if (value.keys.toList.contains(w)) acc + 1 else acc
-      }, wordsToSearch.length)
-
-      fileName -> fileOccurencePercentage
+      val countWords = wordsToSearch.map(w => value.get(w)).map {
+        case Some(v) => 1
+        case None => 0
+      }.sum
+       key -> getFileOccurencePercentage(countWords,wordsToSearch.length)
     }
 
     ListMap(FilesOccurence.toSeq.sortWith(_._2 > _._2): _*)
   }
 
   def getFileOccurencePercentage(wordsFoundInFile: Int, wordsToSearchSize: Int): Double = {
-    val occurencePercentage = (wordsFoundInFile * 100.00) / wordsToSearchSize
+    val occurencePercentage = (wordsFoundInFile * 100.00) / wordsToSearchSize.toDouble
     occurencePercentage
   }
 
